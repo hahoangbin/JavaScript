d02b2e938832073f60998cbc1d097e9247130737
*Thanh <*thanhminh339968az@gmail.com*>
2022-08-25 10:12:52 +0700

    kim tự tháp

---
+++ Bai-75 Prromise/app.js
@@ -0,0 +1,35 @@
+// 3. Promise 
+//    -Sync --> ok
+//    -Async --> ok
+//    -Nỗi đau
+//    -Lý thuyết, cách hoạt động
+//    -Thực hành,ví dụ
+
+setTimeout(function() {
+    console.log(1); //viec1
+    setTimeout(function() {
+        console.log(1); //viec2
+        setTimeout(function() {
+            console.log(1); //viec3
+            setTimeout(function() {
+                console.log(1); //viec4
+                setTimeout(function() {
+                    console.log(1); //viec5
+                    setTimeout(function() {
+                        console.log(1); //viec6
+                        setTimeout(function() {
+                            console.log(1); //viec7
+                            setTimeout(function() {
+                                console.log(1); //viec8
+                                setTimeout(function() {
+                                    console.log(1); //viec9
+                                    
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
