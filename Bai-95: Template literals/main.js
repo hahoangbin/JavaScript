4fc0152da9c138caf870b83efa946d3b71ba5b1a
*Thanh <*thanhminh339968az@gmail.com*>
2022-08-26 14:14:36 +0700

    add

---
+++ Bai-95: Template literals/main.js
@@ -0,0 +1,16 @@
+// Template literals
+
+const courseName = 'JS';
+const courseName2 = 'PHP';
+const description = `Template string noi suy voi: \${}`;
+
+console.log(description);
+
+const lines = 'Line 1\n'
+    + 'Line 2\n'
+    + 'Line 3\n'
+    + 'Line 4\n'
+    + 'Line 5\n'
+
+console.log(lines);
+
