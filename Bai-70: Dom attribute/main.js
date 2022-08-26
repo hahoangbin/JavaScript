197592085b77d06ff0133de2b44c855e4287feb5
*Thanh <*thanhminh339968az@gmail.com*>
2022-08-26 14:55:27 +0700

    add

---
+++ Bai-70: Dom attribute/main.js
@@ -0,0 +1,7 @@
+var headingElement = document.querySelector('h1')
+headingElement.title = 'heading'
+headingElement.className = 'heading-text'
+console.log(headingElement)
+
+console.log(link.getAttribute('class'))
+console.log(link.getAttribute('href'))
