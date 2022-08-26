e97f1d31507c0cdfe47328c0707327b46b11ab61
*Thanh <*thanhminh339968az@gmail.com*>
2022-08-26 14:43:53 +0700

    add

---
+++ Bai-96: Classes/main.js
@@ -0,0 +1,30 @@
+//function Course(name, price) {
+//    this.name = name;
+//    this.price = price;
+//}
+
+class Course {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+
+        this.getName = function() {
+            return this.name;
+        }
+        getName() 
+            return this.name;
+        }
+    }
+
+class Course {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
+const phpCourse = new Course('PHP', 1000);
+const jsCourse = new Course('Javascript', 1200);
+
+console.log(phpCourse);
+console.log(jsCourse);
