a714daf0159f9cd6536f5c4541b558138eb4194d
*Thanh <*thanhminh339968az@gmail.com*>
2022-08-26 10:44:15 +0700

    add

---
+++ Bai-94: Arrow function/main.js
@@ -0,0 +1,27 @@
+//  Arrow function
+
+const logger = (log) => {
+    console.log(log);
+}
+logger('Message...');
+
+const sum = (a,b) => a + b;
+console.log(sum(2,2));
+
+const course = {
+    name: 'Javascript basic',
+    getName: function() {
+        return this; // context
+    }
+};
+
+console.log(course.getName());
+
+const Course = function(name, price) {
+    this.name = name;
+    this.price = price;
+}
+
+const jsCourse = new Course('Javascript', 1000);
+
+console.log(jsCourse);
